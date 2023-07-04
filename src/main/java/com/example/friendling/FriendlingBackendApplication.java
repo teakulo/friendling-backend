ce0b8a92a764add6e525c580336110fce4206330
teakulo <teakul02@gmail.com>
2023-07-04 14:06:05 +0200

    "init commit"

---
+++ src/main/java/com/example/friendling/FriendlingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.friendling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/friendling/FriendlingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.friendling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendlingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
